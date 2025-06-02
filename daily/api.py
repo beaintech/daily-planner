2d603063a72065499f294518f788f70fe4ec2b72
beaintech <beatrixyublome@gmail.com>
2025-06-02 08:09:58 +0200

    api link

--- daily/api.py
+++ daily/api.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://beaintech.github.io/daily-planner/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
